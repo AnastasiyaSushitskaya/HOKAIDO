07a2f6884f08b05e220fb518bf7a679e1e291952
Pon4ikx <p.boeva2510@gmail.com>
2025-01-12 23:04:52 +0300

    настройка url

--- hokaido/specialists/urls.py
+++ hokaido/specialists/urls.py
@@ -3,8 +3,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('main/', views.index, name='index'),  # Главная страница
+    path('main/', views.index, name='index'),
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-
+    path('schedule/', views.schedule_view, name='schedule'),
+    path('menu/', views.menu_view, name='menu'),
+    path('mini_games/', views.mini_games_view, name='mini_games'),
+    path('specialists/', views.specialists_view, name='specialists'),
+    path('clue/', views.clue_view, name='clue'),
+    path('personal/', views.personal_view, name='personal'),
 
 ]
